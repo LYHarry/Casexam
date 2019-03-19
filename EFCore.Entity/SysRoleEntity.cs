dce177fe422703be5d34d26a2271e6bc13062e7a
刘兵 <liubing@bestwise.cc>
2019-03-19 14:44:38 +0800

    添加项目代码

---
+++ EFCore.Entity/SysRoleEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFCore.Entity
+{
+    /// <summary>
+    /// 系统角色
+    /// </summary>
+    [Table("SysRole", Schema = "public")]
+    public class SysRoleEntity
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        [Key]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ EFCore.Entity/SysUserEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFCore.Entity
+{
+    /// <summary>
+    /// 系统管理员用户
+    /// </summary>
+    [Table("SysUser", Schema = "public")]
+    public class SysUserEntity
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        [Key]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 角色ID
+        /// </summary>
+        public int RoleID { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+    }
+}
